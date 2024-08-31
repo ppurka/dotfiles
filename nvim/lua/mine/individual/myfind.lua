028312cfd15eab9dadacb9773e214d9817e0747b
P Purkayastha <ppurka@gmail.com>
2024-08-31 19:38:29 +0100

    original lua code works now

--- nvim/lua/mine/individual/myfind.lua
+++ nvim/lua/mine/individual/myfind.lua
@@ -1,13 +1,6 @@
 local M = {}
 
 M = function()
-    -- original code
-    --local ok = pcall(require"telescope.builtin".git_files, {theme = "dropdown"})
-    --if not ok then require"telescope.builtin".find_files({theme = "dropdown", no_ignore = true}) end
-    --
-    -- All the dance below because require"telescope.builtin".git_files()
-    -- does not honor theme=dropdown
-    -- Telescope command does
     local ok = false
     if vim.g.have_git then
         local fh = assert(io.popen('git status --untracked-files=no --ignore-submodules=all 2>&1 | head -n 1'))
@@ -15,9 +8,11 @@ M = function()
         fh:close()
     end
     if ok then
-        vim.cmd([[Telescope git_files theme=dropdown]])
+        --vim.cmd([[Telescope git_files theme=dropdown]])
+        pcall(require("telescope.builtin").git_files, {theme = "dropdown"})
     else
-        vim.cmd([[Telescope find_files theme=dropdown no_ignore=true]])
+        --vim.cmd([[Telescope find_files theme=dropdown no_ignore=true]])
+        pcall(require("telescope.builtin").find_files, {theme = "dropdown", no_ignore = true})
     end
 end
 
