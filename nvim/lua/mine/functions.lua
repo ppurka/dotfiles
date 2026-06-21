6bbe722fa7f0effabc6a699c0f897baf54c7e834
P Purkayastha <ppurka@gmail.com>
2026-06-21 14:45:34 +0100

    update to newer API and avoid neovide for some keymaps

--- nvim/lua/mine/functions.lua
+++ nvim/lua/mine/functions.lua
@@ -4,7 +4,7 @@ vim.api.nvim_create_user_command(
     'AppendModeline',
     function ()
         local go = vim.api.nvim_get_option_value
-        local et = go("expandtab") and " et" or ""
+        local et = go("expandtab", {}) and " et" or ""
         local modeline = "vim: set" ..
                          " ts = "   .. go("tabstop", {})        ..
                          " sw = "   .. go("shiftwidth", {})     ..
